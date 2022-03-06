8078548f236e5548e41c6c3b870853fd8fa2bc65
mikelmulti460 <mikelenrique@hotmail.com>
2022-03-06 16:56:02 -0500

    Added parameters of class

--- fastApi/main.py
+++ fastApi/main.py
@@ -1,8 +1,40 @@
-from fastapi import FastAPI
+#Python
+from lib2to3.pgen2.token import OP
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastApi
+from fastapi import FastAPI, Body, Query
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 @app.get('/')
 def home():
     return {"Hello": "World"}
 
+# Request and response Body
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    ''' This function create new person in API data '''
+    return person
+
+#validations: Query Parameters
+@app.get("/person/detail")
+def show_person(
+        name:Optional[str] = Query(None, min_length=1, max_length=60),
+        age: int = Query(..., ge=0, le=120)
+    ):
+    return {name: age}
